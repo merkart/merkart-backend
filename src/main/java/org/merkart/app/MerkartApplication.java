0775f7cd37a02356b62264a62f14f40b6df93d77
Desarik <daniel.mejia@masiv.com>
2021-11-03 20:27:48 -0500

    allow all origins CORS

--- src/main/java/org/merkart/app/MerkartApplication.java
+++ src/main/java/org/merkart/app/MerkartApplication.java
@@ -2,13 +2,26 @@ package org.merkart.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MerkartApplication {
 
 	public static void main(String... args) {
 		SpringApplication.run(MerkartApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
